cc048b4119bdab70833823bfd3c99b7d5870718a
LSouberbielle <lucianosouberbielle@mi.unc.edu.ar>
2022-06-24 21:32:31 -0300

    Reentrega
    Tenía un -1 en el load next scene en vez de una +1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
     }
     private void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
 }
